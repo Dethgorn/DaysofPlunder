36cce55224c0a616ec0ab3a728ef913393a3df24
Dethgorn <43420567+Dethgorn@users.noreply.github.com>
2019-04-03 22:52:05 -0700

    Starting the project up

---
+++ Finality/Assets/Scripts/Enemy.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    Rigidbody2D rb;
+
+    public float xSpeed;
+    public float ySpeed;
+
+    public bool canShoot;
+    public float fireRate;
+
+    public float health;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        rb.velocity = new Vector2(xSpeed, ySpeed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            col.gameObject.GetComponent<Player>().Damage();
+            Die();
+        }
+    }
+
+    void Damage()
+    {
+
+        health--;
+        if(health==0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Finality/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    Rigidbody2D rb;
+    public float speed;
+
+    public int health =3;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Use this for initialization
+    void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // move
+        rb.AddForce(new Vector2(Input.GetAxis("Horizontal") * speed, 0));
+        rb.AddForce(new Vector2(0, Input.GetAxis("Vertical") * speed));
+    }
+
+    public void Damage()
+    {
+
+        health--;
+        if(health ==0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
